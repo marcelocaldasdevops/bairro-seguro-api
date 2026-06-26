fddb8ea89f820aa6286e214f84d3b4f0f5385247
marcelo <marcelo.caldas@fpf.br>
2026-06-26 16:29:10 -0400

    feat: refresh do token

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import UserViewSet, IncidentViewSet
 
 router = DefaultRouter()
@@ -7,5 +8,6 @@ router.register(r'users', UserViewSet)
 router.register(r'incidents', IncidentViewSet)
 
 urlpatterns = [
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
